0615fa8afb0bae4bd0d89cb6f6a0693522b6a189
mjduijn <maartenduijn@msn.com>
2020-05-16 14:16:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,44 +6,47 @@ import Title from "./components/Title";
 // ...
 import Pokemon from "./components/Pokemon";
 
-// const props = [
-//   {
-//     name: "Charizard",
-//     weight: 90,
-//     awesome: true,
-//     terrifying: false,
-//     abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
-//   },
-//   {
-//     name: "Bulbasaur",
-//     weight: 6.9,
-//     awesome: true,
-//     terrifying: false,
-//     abilities: ["Overgrow", "Chlorophyll"],
-//   },
-//   {
-//     name: "Mewtwo",
-//     weight: 122,
-//     awesome: true,
-//     terrifying: true,
-//     abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
-//   },
-//   {
-//     name: "Mega beedrill",
-//     weight: 65,
-//     awesome: false,
-//     terrifying: true,
-//     abilities: ["Intimidate", "Unnerve"],
-//   },
-// ];
+const pokemons = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"],
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <main>
         <Title content="Grapje" />
-
-        <Pokemon />
+        {
+            for (pokemon in pokemons) {
+               <Pokemon {...pokemon} />
+            }
+        }
       </main>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />

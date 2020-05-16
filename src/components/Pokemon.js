613a10d5fea10c075a473445566c73a05651255f
mjduijn <maartenduijn@msn.com>
2020-05-16 14:42:10 +0200

    Update Pokemon.js

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -12,3 +12,32 @@ export default function Pokemon(props) {
     </div>
   );
 }
+
+// Alternatief 1 (met lambda)
+const Pokemon = (props) => {
+  return (
+    <div>
+      <h2>Pokemon name: {props.name}</h2>
+      <p>Weight: {props.weight} kg</p>
+      <p>Awesome: {props.awesome ? "YES!" : "nope, not really"}</p>
+      <p>Terrifying: {props.terrifying ? "Very" : "nah, lovable"}</p>
+      <p>Abilities: {props.abilities}</p>
+    </div>
+  );
+}
+export default Pokemon;
+
+
+// Alternatief 2 (met variable destructuring)
+const Pokemon = ({name, weight, awesome, terrigying, abilities}) => {
+  return (
+    <div>
+      <h2>Pokemon name: {name}</h2>
+      <p>Weight: {weight} kg</p>
+      <p>Awesome: {awesome ? "YES!" : "nope, not really"}</p>
+      <p>Terrifying: {terrifying ? "Very" : "nah, lovable"}</p>
+      <p>Abilities: {abilities}</p>
+    </div>
+  );
+}
+export default Pokemon;

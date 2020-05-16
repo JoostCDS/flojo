8a82b3c2ed864e23d5cc6af8669eae02080d6d24
JoostCDS <joost@codaisseur.com>
2020-05-16 14:08:23 +0200

    blob

--- src/App.js
+++ src/App.js
@@ -2,26 +2,51 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Title from "./components/Title";
+// src/App.js
+// ...
+import Pokemon from "./components/Pokemon";
+
+// const props = [
+//   {
+//     name: "Charizard",
+//     weight: 90,
+//     awesome: true,
+//     terrifying: false,
+//     abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+//   },
+//   {
+//     name: "Bulbasaur",
+//     weight: 6.9,
+//     awesome: true,
+//     terrifying: false,
+//     abilities: ["Overgrow", "Chlorophyll"],
+//   },
+//   {
+//     name: "Mewtwo",
+//     weight: 122,
+//     awesome: true,
+//     terrifying: true,
+//     abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+//   },
+//   {
+//     name: "Mega beedrill",
+//     weight: 65,
+//     awesome: false,
+//     terrifying: true,
+//     abilities: ["Intimidate", "Unnerve"],
+//   },
+// ];
 
 function App() {
   return (
     <div className="App">
       <main>
-        <Title />
+        <Title content="Grapje" />
+
+        <Pokemon />
       </main>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> BKALBALBLA save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Helloo World
-        </a>
       </header>
     </div>
   );
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,6 @@
 // src/components/Title.js
 import React from "react";
 
-export default function Title() {
-  return <h1>Hi there!</h1>;
+export default function Title(props) {
+  return <p>{props.content}</p>;
 }
